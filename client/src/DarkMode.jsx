29b405bd61074a94605a961e1e9cf112ed1e2655
Ankit776686 <ankitkumarmanjhi298@gmail.com>
2025-04-18 13:45:22 +0530

    Error Solve Successfuly

--- client/src/DarkMode.jsx
+++ client/src/DarkMode.jsx
@@ -6,7 +6,7 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 const DarkMode = () => {
   const [setTheme] = ["dark", "light", "system"]/// this line self defined
 
-  //Edit
+  //Edits
   return (
     
       <DropdownMenu>
