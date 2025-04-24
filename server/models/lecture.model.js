2be2daf70798e8e208bcdfc9cd985fd9d55a498c
Dhanjee Gond <dgond62063@gmail.com>
2025-04-24 22:26:27 +0530

    Update lecture.model.js Lecture.Model.js  Change

--- server/models/lecture.model.js
+++ server/models/lecture.model.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
 
 const lectureSchema = new mongoose.Schema({
-    lectureTitle:{
+    lectureTitle: {
         type: String,
         required: true
     },
-    videoUrl:{type: String},
-    publicId: {type: String},
-    isPreviewFree: {type: Boolean},
-},{timestamps:true});
+    videoUrl: { type: String },
+    publicId: { type: String },
+    isPreviewFree: { type: Boolean }
+}, { timestamps: true });
 
 export const Lecture = mongoose.model("Lecture", lectureSchema);
